1461821a3507a94f3224dc89161d46d1353bc7b9
yullcha <yuliana.ozerchuk.023@pslib.cz>
2025-05-11 12:47:19 +0200

    :)

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let hamelmnts = document.querySelectorAll(".hamburger-zone");
-    for (const btn of document.querySelectorAll(".hamburger-btn")) {
-        btn.addEventListener("click", (e) => {
-            e.preventDefault();
-            for (const element of hamelmnts) {
-                element.classList.toggle("active");
-            }  
-        });
-    }
-});
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+  }
+  
