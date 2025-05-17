532484402279a4a08c648572b57f0cf6ff183b3f
yullcha <yuliana.ozerchuk.023@pslib.cz>
2025-05-17 10:57:38 +0200

    h

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-    document.querySelector(".nav-links").classList.toggle("active");
-  }
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  toggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show");
+  });
+});
